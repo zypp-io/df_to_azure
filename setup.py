de167b82cdf3a1c4f1811244ceb15e526f42fec4
Erfan Nariman <erfannariman@gmail.com>
2020-12-20 21:46:54 +0100

    release 0.1.0

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open("requirements.txt") as fp:
     install_requires = fp.read()
 
 setup(
-    name="df_to_azure",  # Replace with your own username
-    version="0.0.5",
+    name="df_to_azure",
+    version="0.1.0rc1",
     author="Melvin Folkers",
     author_email="melvin@zypp.io",
     description="Automatically create pipelines with copy activity from blob to SQL",
@@ -18,13 +18,12 @@ setup(
     url="https://github.com/zypp-io/df_to_azure",
     packages=find_packages(),
     install_requires=install_requires,
-    entry_points={"console_scripts": ["run=df_to_azure.export:run"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     project_urls={
         "Bug Reports": "https://github.com/zypp-io/df_to_azure/issues",
         "Source": "https://github.com/zypp-io/df_to_azure",
