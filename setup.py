0f4190993c1a3818570c76f9b2ffba9b33b7d146
Erfan Nariman <erfannariman@gmail.com>
2021-01-15 20:45:29 +0100

    release 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fp:
 
 setup(
     name="df_to_azure",
-    version="0.1.0",
+    version="0.1.1",
     author="Melvin Folkers, Erfan Nariman",
     author_email="melvin@zypp.io, erfan@zypp.io",
     description="Automatically create pipelines with copy activity from blob to SQL",
