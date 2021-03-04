2ea0d8cfe9434eb0b2ccf1e753981bdd6c612802
Erfan Nariman <erfannariman@gmail.com>
2021-03-04 13:24:41 +0100

    fix black

--- df_to_azure/tests/test_df_to_azure.py
+++ df_to_azure/tests/test_df_to_azure.py
@@ -363,7 +363,7 @@ def test_clean_up_db():
             "sample_2",
             "test_df_to_azure",
             "wrong_method",
-            "long_string"
+            "long_string",
         ],
     }
 
