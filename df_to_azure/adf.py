b4ea399d8edfe7a5554939128a4b7de0033e69f7
Erfan Nariman <erfannariman@gmail.com>
2021-03-30 11:11:51 +0200

    add backslash n as rowdelimiter

--- df_to_azure/adf.py
+++ df_to_azure/adf.py
@@ -134,7 +134,7 @@ def create_input_blob(table):
         format={
             "type": "TextFormat",
             "columnDelimiter": "^",
-            "rowDelimiter": "",
+            "rowDelimiter": "\n",
             "treatEmptyAsNull": "true",
             "skipLineCount": 0,
             "firstRowAsHeader": "true",
