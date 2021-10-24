e5dbc85e11faf0c3361725ecd04f283a74565fe7
Erfan Nariman <erfannariman@gmail.com>
2021-10-24 20:27:15 +0200

    add test for uniqueness

--- df_to_azure/tests/test_parquet.py
+++ df_to_azure/tests/test_parquet.py
@@ -2,6 +2,7 @@ import os
 from io import BytesIO
 from time import sleep
 
+import pytest
 from azure.storage.blob import BlobServiceClient
 from pandas import DataFrame, read_parquet
 from pandas.testing import assert_frame_equal
@@ -26,25 +27,33 @@ def test_append_parquet():
 def test_upsert_parquet_same_shape():
     df = DataFrame({"id": range(1000, 1050, 10), "value1": range(10, 60, 10), "value2": list("abcde")})
     # upload original df to storage
-    df_to_azure(df=df, tablename="test_upsert", schema="test_parquet", parquet=True)
+    df_to_azure(df=df, tablename="upsert", schema="test_parquet", parquet=True)
 
     # make updates to table
-    df = df.copy()
     df.loc[0, "value1"] = 99
     df.loc[3, "value2"] = "ZZ"
 
     # perform upsert
-    df_to_azure(df=df, tablename="test_upsert", schema="test_parquet", parquet=True)
+    df_to_azure(df=df, tablename="upsert", schema="test_parquet", method="upsert", parquet=True, id_field=["id"])
 
     # download the parquet back
     blob_service_client = BlobServiceClient.from_connection_string(os.environ.get("AZURE_STORAGE_CONNECTION_STRING"))
     container_client = blob_service_client.get_container_client(container="parquet")
-    downloaded_blob = container_client.download_blob("test_parquet/test_upsert.parquet")
+    downloaded_blob = container_client.download_blob("test_parquet/upsert.parquet")
     bytes_io = BytesIO(downloaded_blob.readall())
     result = read_parquet(bytes_io)
 
     # check if upsert was successful
     assert_frame_equal(df, result)
+
+
+def test_uniqueness_id_cols():
+    df = DataFrame({"id": range(1000, 1050, 10), "value1": range(10, 60, 10), "value2": list("abcde")})
+    df.loc[1, "id"] = df.loc[0, "id"]
+
+    # this should raise since columns are not unique
+    with pytest.raises(AssertionError):
+        df_to_azure(df=df, tablename="test_upsert", schema="test_parquet", method="upsert", id_field=["id"])
 
 
 if __name__ == "__main__":
