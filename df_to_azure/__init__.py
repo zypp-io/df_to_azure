0e8d24638614f2a982619913548fbaed58a54631
Tim van der Heijden <tim@zypp.io>
2022-07-14 12:04:18 +0200

    Up version

--- df_to_azure/__init__.py
+++ df_to_azure/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from .export import df_to_azure
 
-__version__ = "0.7.0-rc.2"
+__version__ = "0.7.1-rc.1"
 
 logging.basicConfig(
     format="%(asctime)s.%(msecs)03d [%(levelname)-5s] [%(name)s] - %(message)s",
